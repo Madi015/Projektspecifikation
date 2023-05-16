94ac141f25c7362d538f79322c28bcbab9a07ebd
Madi015 <madisaed2003@yahoo.com>
2023-05-16 22:48:09 +0200

    Nu kan programmet hitta rätt person och räkna arbetstimmar för varje dag och lägg till den till en specifik lista som tillhöt samma person.

--- Projektspecifikation.py
+++ Projektspecifikation.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 import json
 Madi  = []
 David = []
@@ -9,5 +9,21 @@ with open('in_ut.json', 'r') as filereader:
     allData = json.loads(Data)
 for X_dic in allData:
     if X_dic["namn"] == "Madi":
-        print(X_dic)
-          
+        inTime = datetime.strptime(X_dic["incheckning"], '%Y-%m-%dT%H:%M:%S')
+        utTime = datetime.strptime(X_dic["utcheckning"], '%Y-%m-%dT%H:%M:%S')
+        # Rakna ut antal timmar
+        diffTime = timedelta(hours=(utTime.hour - inTime.hour), minutes=(utTime.minute - inTime.minute), seconds=(utTime.second - inTime.second))
+        # addera till en list som tillhor samma person 
+        Madi.append(diffTime)
+    elif X_dic["namn"] == "David":
+        inTime = datetime.strptime(X_dic["incheckning"], '%Y-%m-%dT%H:%M:%S')
+        utTime = datetime.strptime(X_dic["utcheckning"], '%Y-%m-%dT%H:%M:%S')
+        diffTime = timedelta(hours=(utTime.hour - inTime.hour), minutes=(utTime.minute - inTime.minute), seconds=(utTime.second - inTime.second))
+        David.append(diffTime)
+    elif X_dic["namn"] == "Mia":
+        inTime = datetime.strptime(X_dic["incheckning"], '%Y-%m-%dT%H:%M:%S')
+        utTime = datetime.strptime(X_dic["utcheckning"], '%Y-%m-%dT%H:%M:%S')
+        diffTime = timedelta(hours=(utTime.hour - inTime.hour), minutes=(utTime.minute - inTime.minute), seconds=(utTime.second - inTime.second))
+        Mia.append(diffTime)
+    else:
+        print("Personen finns inte med i vår system.")
