24b3545db27341f437a4ab900027d7a8b08c8a2a
Madi015 <madisaed2003@yahoo.com>
2023-05-16 14:20:06 +0200

    Nu kan jag öpnna json filen, läsa av data samt plocka vilken person vill jag ha. defenirat tre listor för tre anställda.

--- Projektspecifikation.py
+++ Projektspecifikation.py
@@ -1,1 +1,13 @@
-print('hello World')
+import datetime
+import json
+Madi  = []
+David = []
+Mia   = []
+
+with open('in_ut.json', 'r') as filereader:
+    Data = filereader.read()
+    allData = json.loads(Data)
+for X_dic in allData:
+    if X_dic["namn"] == "Madi":
+        print(X_dic)
+          
